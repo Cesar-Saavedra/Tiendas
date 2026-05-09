70dd224cf7823258079d60e6aef9d156658a4e73
César Saavedra C. <ces.saavedra@duocuc.cl>
2026-05-09 17:05:11 -0400

    Cesar
    Primer Commit

---
+++ src/test/java/cl/duoc/ms_tiendas/MsTiendasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_tiendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTiendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
