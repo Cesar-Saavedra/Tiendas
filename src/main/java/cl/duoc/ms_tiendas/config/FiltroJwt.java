f150b39c9db55cb0ea4e4a7c7a196541be72617f
TomasB <to.blanchard@duocuc.cl>
2026-05-16 13:32:24 -0400

    Actualizacion tiendas
    dto, imports unused

--- src/main/java/cl/duoc/ms_tiendas/config/FiltroJwt.java
+++ src/main/java/cl/duoc/ms_tiendas/config/FiltroJwt.java
@@ -1,26 +1,24 @@
 package cl.duoc.ms_tiendas.config;
 
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Value;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.security.Key;
-import java.util.List;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
 
 @Component
 public class FiltroJwt extends OncePerRequestFilter{
